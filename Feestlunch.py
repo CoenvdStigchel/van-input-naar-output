e106983969ad2825866f58287cfaeb79e717a409
School <school@MacBook-Air-van-Coen.local>
2021-09-16 09:31:54 +0200

    van-input-naar-output

---
+++ Feestlunch.py
@@ -0,0 +1,20 @@
+PrijsCroissantjes=0.39
+AantalCroissantjes = int(input('Hoeveel Croissantjes wil je hebben? '))
+
+PrijsStokbrooden=2.78
+AantalStokbrooden = int(input('Hoeveel stokbrooden wil je hebben? '))
+
+PrijsKortingsbonnen=0.50
+AantalKortingsbonnen = int(input('Hoeveel kortingsbonnen heb je? '))
+
+print(PrijsCroissantjes*AantalCroissantjes+PrijsStokbrooden*AantalStokbrooden-PrijsKortingsbonnen*AantalKortingsbonnen)
+
+factuurtekst= ("U heeft " + str(AantalCroissantjes) + str(AantalStokbrooden) ("en u heeft ") + str(AantalKortingsbonnen))
+
+
+print(factuurtekst)
+
+
+
+
+
---
+++ speelhaldag.py
@@ -0,0 +1,14 @@
+from ast import Str
+
+
+Ticket = 7.45
+aantalTickets = int(input('Hoeveel tickets wil je hebben? '))
+GameSeat = 0.37
+periodesvan5min = int(input('Hoeveel Periodes van 5 minuten op de VR GameSeat wil je hebben? '))
+aantalPersonen = int(input('Voor hoeveel mensen wil je reserveren? '))
+
+Bedrag = Ticket*aantalTickets+GameSeat*periodesvan5min+aantalPersonen
+
+factuurTekst = "Dit geweldige dagje uit met " + str(aantalPersonen) + " mensen in de Speelhal met " + str(periodesvan5min) + " spellen van 5 minuten lang op de VR GameSeat kost je maar " + str( Bedrag) + " euro. Goedkoper krijg je niet! Ga snel naar speelhaldavinci.nl!"
+
+print(factuurTekst)
